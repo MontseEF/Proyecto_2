85ec1761e9cdcd1e36366fdd9c018750a1a6afab
MontseEF <monfloresespinoza29@gmail.com>
2026-01-21 19:05:19 -0300

    first_commit

---
+++ main.py
@@ -0,0 +1,3 @@
+#Lista pacientes (guardara los datos de los pacientes)
+patients = []
+next_patient_id = 1  # Variable para asignar IDs únicos a los pacientes
---
+++ modules/menu.py
@@ -0,0 +1,12 @@
+#creación de menú
+
+def show_menu():
+    print("=== Menú Principal ===")
+    print(" BIENVENIDOS A LA AGENDA VETERINARIA")
+    print("1. Agregar nuevo paciente")
+    print("2. Consultar agenda")
+    print("3. Agendar cita")
+    print("4. Cancelar cita")
+    print("5. Listado pacientes")
+    print("6. Buscar paciente")
+    print("7. Salir")
